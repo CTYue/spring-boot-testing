9ed296916da253316b20d476142dc58f1d4bfad5
TAIsRich <Richard.lyu0120@gmail.com>
2022-07-11 00:53:26 -0700

    v1.3.0-controller: test RUD

--- src/main/java/com/chuwa/springboottesting/controller/EmployeeController.java
+++ src/main/java/com/chuwa/springboottesting/controller/EmployeeController.java
@@ -4,7 +4,10 @@ import com.chuwa.springboottesting.model.Employee;
 import com.chuwa.springboottesting.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * @author b1go
@@ -21,5 +24,43 @@ public class EmployeeController {
     @ResponseStatus(HttpStatus.CREATED)
     public Employee createEmployee(@RequestBody Employee employee) {
         return employeeService.saveEmployee(employee);
+    }
+
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        return employeeService.getAllEmployees();
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable("id") long employeeId){
+        return employeeService.getEmployeeById(employeeId)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable("id") long employeeId,
+                                                   @RequestBody Employee employee){
+        return employeeService.getEmployeeById(employeeId)
+                .map(savedEmployee -> {
+
+                    savedEmployee.setFirstName(employee.getFirstName());
+                    savedEmployee.setLastName(employee.getLastName());
+                    savedEmployee.setEmail(employee.getEmail());
+
+                    Employee updatedEmployee = employeeService.updateEmployee(savedEmployee);
+                    return new ResponseEntity<>(updatedEmployee, HttpStatus.OK);
+
+                })
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteEmployee(@PathVariable("id") long employeeId){
+
+        employeeService.deleteEmployee(employeeId);
+
+        return new ResponseEntity<String>("Employee deleted successfully!.", HttpStatus.OK);
+
     }
 }
--- src/test/java/com/chuwa/springboottesting/controller/EmployeeControllerTest.java
+++ src/test/java/com/chuwa/springboottesting/controller/EmployeeControllerTest.java
@@ -3,24 +3,26 @@ package com.chuwa.springboottesting.controller;
 
 import com.chuwa.springboottesting.model.Employee;
 import com.chuwa.springboottesting.service.EmployeeService;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentMatchers;
-import org.mockito.BDDMockito;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 /**
  * @author b1go
@@ -45,16 +47,22 @@ public class EmployeeControllerTest {
     @Resource
     private ObjectMapper objectMapper;
 
-    @Test
-    public void testCreateEmployee() throws Exception {
+    private Employee employee;
 
-        //given - precondition or setup
-        Employee employee = Employee.builder()
+    @BeforeEach
+    void setUp() {
+        employee = Employee.builder()
                 .firstName("chuwa")
                 .lastName("america")
                 .email("chuwa@gmail.com")
                 .build();
-        BDDMockito.given(employeeService.saveEmployee(ArgumentMatchers.any(Employee.class)))
+    }
+
+    @Test
+    public void testCreateEmployee() throws Exception {
+
+        //given - precondition or setup
+        given(employeeService.saveEmployee(any(Employee.class)))
                 .willAnswer((invocation) -> invocation.getArgument(0));
 
         // when - action or the behaviour that we are going test
@@ -63,10 +71,133 @@ public class EmployeeControllerTest {
                 .content(objectMapper.writeValueAsString(employee)));
 
         // then - verify the output
-        res.andDo(MockMvcResultHandlers.print())
-                .andExpect(MockMvcResultMatchers.status().isCreated())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.firstName", CoreMatchers.is(employee.getFirstName())))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.lastName", CoreMatchers.is(employee.getLastName())))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.email", CoreMatchers.is(employee.getEmail())));
+        res.andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.firstName", is(employee.getFirstName())))
+                .andExpect(jsonPath("$.lastName", is(employee.getLastName())))
+                .andExpect(jsonPath("$.email", is(employee.getEmail())));
+    }
+
+    @Test
+    public void testGetAllEmployees() throws Exception {
+
+        //given - precondition or setup
+        List<Employee> employees = new ArrayList<>();
+        employees.add(employee);
+        employees.add(Employee.builder().firstName("chuwa1").lastName("america1").email("chuwa1@gmail.com").build());
+        given(employeeService.getAllEmployees()).willReturn(employees);
+
+        // when - action or the behaviour that we are going test
+        ResultActions response = mockMvc.perform(get("/api/v1/employees"));
+
+        // then - verify the output
+        response.andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$.size()", is(employees.size())));
+    }
+
+    // positive scenario - valid employee id
+    // JUnit test for GET employee by id REST API
+    @Test
+    public void testGetByEmployeeId() throws Exception {
+        // given - precondition or setup
+        long employeeId = 1L;
+        given(employeeService.getEmployeeById(employeeId)).willReturn(Optional.of(employee));
+
+        // when -  action or the behaviour that we are going test
+        ResultActions response = mockMvc.perform(get("/api/v1/employees/{id}", employeeId));
+
+        // then - verify the output
+        response.andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$.firstName", is(employee.getFirstName())))
+                .andExpect(jsonPath("$.lastName", is(employee.getLastName())))
+                .andExpect(jsonPath("$.email", is(employee.getEmail())));
+
+    }
+
+    // negative scenario - valid employee id
+    // JUnit test for GET employee by id REST API
+    @Test
+    public void testInvalidEmployeeId_ReturnEmpty() throws Exception {
+        // given - precondition or setup
+        long employeeId = 1L;
+        given(employeeService.getEmployeeById(employeeId)).willReturn(Optional.empty());
+
+        // when -  action or the behaviour that we are going test
+        ResultActions response = mockMvc.perform(get("/api/v1/employees/{id}", employeeId));
+
+        // then - verify the output
+        response.andExpect(status().isNotFound())
+                .andDo(print());
+    }
+
+    // JUnit test for update employee REST API - positive scenario
+    @Test
+    public void testUpdatedEmployee() throws Exception {
+        // given - precondition or setup
+        long employeeId = 1L;
+
+        Employee updatedEmployee = Employee.builder()
+                .firstName("John")
+                .lastName("Snow")
+                .email("John@gmail.com")
+                .build();
+        given(employeeService.getEmployeeById(employeeId)).willReturn(Optional.of(employee));
+        given(employeeService.updateEmployee(any(Employee.class)))
+                .willAnswer((invocation) -> invocation.getArgument(0));
+
+        // when -  action or the behaviour that we are going test
+        ResultActions response = mockMvc.perform(put("/api/v1/employees/{id}", employeeId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(updatedEmployee)));
+
+
+        // then - verify the output
+        response.andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$.firstName", is(updatedEmployee.getFirstName())))
+                .andExpect(jsonPath("$.lastName", is(updatedEmployee.getLastName())))
+                .andExpect(jsonPath("$.email", is(updatedEmployee.getEmail())));
+    }
+
+    // JUnit test for update employee REST API - negative scenario
+    @Test
+    public void testUpdatedEmployee_Return404() throws Exception {
+        // given - precondition or setup
+        long employeeId = 1L;
+
+        Employee updatedEmployee = Employee.builder()
+                .firstName("John")
+                .lastName("Snow")
+                .email("John@gmail.com")
+                .build();
+        given(employeeService.getEmployeeById(employeeId)).willReturn(Optional.empty());
+        given(employeeService.updateEmployee(any(Employee.class)))
+                .willAnswer((invocation) -> invocation.getArgument(0));
+
+        // when -  action or the behaviour that we are going test
+        ResultActions response = mockMvc.perform(put("/api/v1/employees/{id}", employeeId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(updatedEmployee)));
+
+        // then - verify the output
+        response.andExpect(status().isNotFound())
+                .andDo(print());
+    }
+
+    // JUnit test for delete employee REST API
+    @Test
+    public void testDeleteByEmployeeId_Return200() throws Exception {
+        // given - precondition or setup
+        long employeeId = 1L;
+        willDoNothing().given(employeeService).deleteEmployee(employeeId);
+
+        // when -  action or the behaviour that we are going test
+        ResultActions response = mockMvc.perform(delete("/api/v1/employees/{id}", employeeId));
+
+        // then - verify the output
+        response.andExpect(status().isOk())
+                .andDo(print());
     }
 }
