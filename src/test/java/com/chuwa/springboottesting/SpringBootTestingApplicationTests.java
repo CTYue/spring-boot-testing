d566724a872a65cd5c72bf20b16775098e492755
TAIsRich <Richard.lyu0120@gmail.com>
2022-07-10 17:22:37 -0700

    init

---
+++ src/test/java/com/chuwa/springboottesting/SpringBootTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chuwa.springboottesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
